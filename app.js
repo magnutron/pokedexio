92a8a455f8296b795b744854ce87ba187099a256
Mads <mk@magnutron.dk>
2023-03-28 09:57:58 +0200

    02
    02

--- app.js
+++ app.js
@@ -29,26 +29,26 @@ function addPokemon(pokemon) {
   );
 
   document.querySelector("#pokemon article:last-child").addEventListener("click", pokemonClicked);
-}
 
-function pokemonClicked() {
-  console.log(pokemon);
-  document.querySelector("#dialog-name").textContent = `${pokemon.name}`;
-  document.querySelector("#dialog-description").textContent = `${pokemon.name}`;
-  document.querySelector("#dialog-ability").textContent = `Abilities: ${pokemon.ability}`;
-  document.querySelector("#dialog-type").textContent = `Type: c${pokemon.type}`;
-  document.querySelector("#dialog-subtype").textContent = `Sub type: ${pokemon.subtype}`;
-  document.querySelector("#dialog-weaknesses").textContent = `Weaknesses: ${pokemon.weaknesses}`;
-  document.querySelector("#dialog-gender").textContent = `Gender: ${pokemon.gender}`;
-  document.querySelector("#dialog-weight").textContent = `Weight: ${pokemon.weight}`;
-  document.querySelector("#dialog-height").textContent = `Height: ${pokemon.height}`;
-  document.querySelector("#dialog-generation").textContent = `Generation: ${pokemon.generation}`;
-  document.querySelector("#dialog-statsHP").textContent = `HP: ${pokemon.statsHP}`;
-  document.querySelector("#dialog-statsAttack").textContent = `Attack: ${pokemon.statsAttack}`;
-  document.querySelector("#dialog-statsDefence").textContent = `Defence: ${pokemon.statsDefence}`;
-  document.querySelector("#dialog-statsSpecialAttack").textContent = `Special Attack: ${pokemon.statsSpecialAttack}`;
-  document.querySelector("#dialog-statsSpecialDefence").textContent = `Special Defence: ${pokemon.statsSpecialDefence}`;
-  document.querySelector("#dialog-statsSpeed").textContent = `Speed: ${pokemon.statsspeed}`;
-  document.querySelector("#dialog-img").src = pokemon.image;
-  document.querySelector("#dialog-pokemon").showModal();
+  function pokemonClicked() {
+    console.log(pokemon);
+    document.querySelector("#dialog-name").textContent = `${pokemon.name}`;
+    document.querySelector("#dialog-img").src = pokemon.image;
+    document.querySelector("#dialog-description").textContent = `${pokemon.description}`;
+    document.querySelector("#dialog-ability").textContent = `Abilities: ${pokemon.ability}`;
+    document.querySelector("#dialog-type").textContent = `Type: c${pokemon.type}`;
+    document.querySelector("#dialog-subtype").textContent = `Sub type: ${pokemon.subtype}`;
+    document.querySelector("#dialog-weaknesses").textContent = `Weaknesses: ${pokemon.weaknesses}`;
+    document.querySelector("#dialog-gender").textContent = `Gender: ${pokemon.gender}`;
+    document.querySelector("#dialog-weight").textContent = `Weight: ${pokemon.weight}`;
+    document.querySelector("#dialog-height").textContent = `Height: ${pokemon.height}`;
+    document.querySelector("#dialog-generation").textContent = `Generation: ${pokemon.generation}`;
+    document.querySelector("#dialog-statsHP").textContent = `HP: ${pokemon.statsHP}`;
+    document.querySelector("#dialog-statsAttack").textContent = `Attack: ${pokemon.statsAttack}`;
+    document.querySelector("#dialog-statsDefence").textContent = `Defence: ${pokemon.statsDefence}`;
+    document.querySelector("#dialog-statsSpecialAttack").textContent = `Special Attack: ${pokemon.statsSpecialAttack}`;
+    document.querySelector("#dialog-statsSpecialDefence").textContent = `Special Defence: ${pokemon.statsSpecialDefence}`;
+    document.querySelector("#dialog-statsSpeed").textContent = `Speed: ${pokemon.statsspeed}`;
+    document.querySelector("#dialog-pokemon").showModal();
+  }
 }
